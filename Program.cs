5b6bdc87cbd9d1dcfd407f2039c84690bdfbd767
ThangNguyen312v <tatthang112113@gmail.com>
2024-08-12 20:36:01 +0700

    baitap

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace laisuatchovay{
+    class Program{
+        static void Main(string[] Args){
+            double money;
+            int month;
+            double intersetRate;
+
+            Console.WriteLine("So tien muon gui: ");
+            money = Double.Parse(Console.ReadLine());
+            Console.WriteLine("So thang muon gui: ");
+            month = Int16.Parse(Console.ReadLine());
+            Console.WriteLine("Lai suat la: ");
+            intersetRate = double.Parse(Console.ReadLine());
+
+            double totalInterset = 0;
+            for(int i = 0; i < month; i++)
+            {
+                totalInterset = money * (intersetRate/100)/12 * 3; 
+            }
+            Console.WriteLine("Lai suat nhan duoc: " + totalInterset);
+
+        }
+    }
+}
